f11a997cb62fd41a0dad51e8fc595b368e7bfc66
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-01-20 18:11:58 -0800

    Update login activity onCreate method

--- app/src/main/java/com/example/messenger/LoginActivity.kt
+++ app/src/main/java/com/example/messenger/LoginActivity.kt
@@ -5,8 +5,10 @@ import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
 }
