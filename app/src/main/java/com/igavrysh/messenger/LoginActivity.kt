26fd0816df11e48849e068c9b12152220e97f0a4
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-01-23 19:33:07 -0800

    Firebase authorization, rounded buttons on Register Activity

--- app/src/main/java/com/igavrysh/messenger/LoginActivity.kt
+++ app/src/main/java/com/igavrysh/messenger/LoginActivity.kt
@@ -2,7 +2,9 @@ package com.igavrysh.messenger
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
@@ -17,6 +19,33 @@ class LoginActivity: AppCompatActivity() {
             val password = password_edittext_login.text.toString()
 
             Log.d("Login", "Attempt login with email/pw: $email/***")
+
+            FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) {
+                        return@addOnCompleteListener
+                    }
+
+                    Log.d(
+                        "Main",
+                        "Successfully created user with uid: $it.result.user.uid")
+
+                    Toast.makeText(
+                        this,
+                        "Successfull login for user with uid: $it.result.user.uid",
+                        Toast.LENGTH_SHORT)
+                        .show()
+
+                }
+                .addOnFailureListener {
+                    Log.d("Main", "Failed to create user: ${it.message}")
+                    Toast.makeText(
+                        this,
+                        "Failed to login user: ${it.message}",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
         }
 
         back_to_register_textview.setOnClickListener {
